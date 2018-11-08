4dad3617530661246783a89d247eb3305430b1c9
amar_deep_singh <amar_deep_singh@192.168.0.3>
2018-11-08 16:41:54 -0600

    another attempt

--- vars/populateTestUrl.groovy
+++ vars/populateTestUrl.groovy
@@ -4,8 +4,14 @@ def call(args) {
       Properties props = new Properties()
 File propsFile = new File('pipeline/config.properties')
 props.load(propsFile.newDataInputStream())
+println props.getProperty('TEST_SERVER_BASE_URL')
+
+
 props.setProperty('TEST_SERVER_BASE_URL', args.toString())
 props.setProperty('TEST_SERVER_BASE_URLK', 'abc')
 
 props.store(propsFile.newWriter(), null)
+
+props.load(propsFile.newDataInputStream())
+println props.getProperty('TEST_SERVER_BASE_URL')
 }
