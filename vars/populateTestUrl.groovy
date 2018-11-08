71c0d80145b9d6692a5dd6744c3990abc2dd64ee
amar_deep_singh <amar_deep_singh@192.168.0.3>
2018-11-08 16:20:11 -0600

    added test url

--- vars/populateTestUrl.groovy
+++ vars/populateTestUrl.groovy
@@ -1,4 +1,9 @@
 #!/usr/bin/env groovy
 def call(args) {
       echo "Global liberary is called -${args}"
+      Properties props = new Properties()
+File propsFile = new File('pipeline/config.properties')
+props.load(propsFile.newDataInputStream())
+props.setProperty('TEST_SERVER_BASE_URL', args.toString())
+props.store(propsFile.newWriter(), null)
 }
