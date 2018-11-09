cc47b769769d5cde5be3b2f2c485d3c1e1c08edc
amar_deep_singh <amar_deep_singh@192.168.0.3>
2018-11-08 23:40:53 -0600

    modified deploy to aws

--- vars/deployToAws.groovy
+++ vars/deployToAws.groovy
@@ -1,20 +1,23 @@
 #!/usr/bin/env groovy
-def call(deploymentUnitName) {
+def call(deploymentUnitName,environment) {
                  def stackExists
+                 def currentUnit="${deploymentUnit}-${environment}"
+                 def stackName ="${currentUnit}-stack"
+                 
                    try{
-                 		stackExists = sh (script: "aws cloudformation describe-stacks --stack-name ${deploymentUnitName}",returnStdout: true)
+                 		stackExists = sh (script: "aws cloudformation describe-stacks --stack-name ${stackName}",returnStdout: true)
 					}catch(Exception e){
 					}
 
                 if(!stackExists) {
                 echo "Creating a new stack"
-                sh "aws cloudformation create-stack --stack-name ${deploymentUnitName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
+                sh "aws cloudformation create-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
                 sh "aws cloudformation wait stack-create-complete --stack-name ${deploymentUnitName}"
                 }else{
                 try{
                 echo "Updating an existing stack"
-                sh "aws cloudformation update-stack --stack-name ${deploymentUnitName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
-                sh "aws cloudformation wait stack-update-complete --stack-name ${deploymentUnitName}"
+                sh "aws cloudformation update-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
+                sh "aws cloudformation wait stack-update-complete --stack-name ${stackName}"
                 }catch(Exception e){
                 		echo 'Updates to existing stack failed !'
                 }
