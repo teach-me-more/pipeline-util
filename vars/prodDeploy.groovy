9aaf00700801b6aaf40e08c4545cfb7bf24b899b
amar_deep_singh <amar_deep_singh@192.168.0.3>
2018-11-09 00:29:25 -0600

    added environment deployment

---
+++ vars/prodDeploy.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+def  call() {
+echo 'Deploying System to PROD environment'
+ deployToAws(deploymentUnitName,"PROD")
+  def prodUrl = populateTestUrl(deploymentUnitName,"PROD")
+ echo "Production System is available at url = ${prodUrl}"
+
+echo 'Deployment Completed successfully !'
+}
---
+++ vars/uatDeploy.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+def  call() {
+echo 'Deploying System to UAT environment'
+            deployToAws(deploymentUnitName,"UAT")
+              def uatUrl = populateTestUrl(deploymentUnitName,"UAT")
+             echo "UAT System is available at url = ${uatUrl}"
+            input message: 'Click to Deploy in Production Environment', ok: 'Deploy To Production'
+}
