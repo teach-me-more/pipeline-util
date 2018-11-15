d83fc9b1c8ede8144d287a98004242555fe0c621
amar_deep_singh <amar_deep_singh@10.88.155.12>
2018-11-14 22:49:25 -0600

    commented upload

--- vars/uploadArtifacts.groovy
+++ vars/uploadArtifacts.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 def call() {
-					sh 'aws s3 sync target/ s3://amar-deep-singh  --exclude "*.*"  --include "aws-code-deploy-0.1.0.jar"'
+				//	sh 'aws s3 sync target/ s3://amar-deep-singh  --exclude "*.*"  --include "aws-code-deploy-0.1.0.jar"'
 }
