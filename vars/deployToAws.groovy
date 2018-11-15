c2ba10f17bdfdaeb8722704f0595173341c7e709
amar_deep_singh <amar_deep_singh@10.88.155.12>
2018-11-14 22:14:49 -0600

    Added sanity check

--- vars/deployToAws.groovy
+++ vars/deployToAws.groovy
@@ -1,25 +1,25 @@
 #!/usr/bin/env groovy
 def call(deploymentUnitName,environment) {
-                 def stackExists
-                 def currentUnit="${deploymentUnitName}-${environment}"
-                 def stackName ="${currentUnit}-stack"
-                 
-                   try{
-                 		stackExists = sh (script: "aws cloudformation describe-stacks --stack-name ${stackName}",returnStdout: true)
-					}catch(Exception e){
-					}
+	def stackExists
+	def currentUnit="${deploymentUnitName}-${environment}"
+	def stackName ="${currentUnit}-stack"
 
-                if(!stackExists) {
-                echo "Creating a new stack"
-                sh "aws cloudformation create-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
-                sh "aws cloudformation wait stack-create-complete --stack-name ${stackName}"
-                }else{
-                try{
-                echo "Updating an existing stack"
-                sh "aws cloudformation update-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
-                sh "aws cloudformation wait stack-update-complete --stack-name ${stackName}"
-                }catch(Exception e){
-                		echo 'Updates to existing stack failed !'
-                }
-                }
+	try{
+		stackExists = sh (script: "aws cloudformation describe-stacks --stack-name ${stackName}",returnStdout: true)
+	}catch(Exception e){
+	}
+
+	if(!stackExists) {
+		echo "Creating a new stack"
+		sh "aws cloudformation create-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
+		sh "aws cloudformation wait stack-create-complete --stack-name ${stackName}"
+	}else{
+		try{
+			echo "Updating an existing stack"
+			sh "aws cloudformation update-stack --stack-name ${stackName} --template-url ${TemplateName} --capabilities CAPABILITY_IAM --parameters ParameterKey=deploymentUnit,ParameterValue=${currentUnit} ParameterKey=MaxNoOfInstances,ParameterValue=${MaxServers} ParameterKey=MinNoOfInstances,ParameterValue=${MinServers}"
+			sh "aws cloudformation wait stack-update-complete --stack-name ${stackName}"
+		}catch(Exception e){
+			echo 'Updates to existing stack failed !'
+		}
+	}
 }
---
+++ vars/performSanityCheck.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/env groovy
+def call(deploymentUnitName,environment) {
+TEST_URL= populateTestUrl(deploymentUnitName,"Dev");
+echo "Application deployed with url - ${TEST_URL}"
+				
+def TEST_RESULT
+def expectedResult = 'Hello World !'
+TEST_RESULT = sh returnStdout: true, script: "curl $TEST_URL"
+if("${expectedResult}"=="${TEST_RESULT}"){
+ echo "Build deployed successfully ! test result== ${TEST_RESULT} "
+ sh "mvn verify -DTEST_SERVER_BASE_URL=${TEST_URL}"
+}else{
+ echo "Build deployment failed  !"
+ fail("Application Sanity chek failed")
+}
+
+}
