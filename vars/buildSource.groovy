76a9a423cb5ab30f3bd14a8a9b54688c9fb1e884
amar_deep_singh <amar_deep_singh@10.88.155.12>
2018-11-14 23:55:12 -0600

    reverted some changes

--- vars/buildSource.groovy
+++ vars/buildSource.groovy
@@ -1,11 +1,6 @@
 #!/usr/bin/env groovy
 def call() {
 	echo 'Compiling source code'
-	mavenInfo = readMavenPom file:''
-	deploymentUnitName = mavenInfo.artifactId
-	version = mavenInfo.version
-	config = readProperties file:'pipeline/config.properties'
-	echo "Deployment unit name = ${deploymentUnitName}"
 
 	withMaven(maven : 'Maven_3.5.4'){ sh 'mvn clean compile' }
 }
