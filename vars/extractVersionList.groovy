b682b59dd07903ef66581703cbd472e21c91d135
chaudharyamardeep <chaudharyamardeep@gmail.com>
2020-01-22 22:41:45 -0600

    complex inputs

--- vars/extractVersionList.groovy
+++ vars/extractVersionList.groovy
@@ -9,7 +9,7 @@ def call(config) {
 	input message: 'Please do something ', ok: 'Hey', parameters: [extendedChoice(bindings: '', description: '', groovyClasspath: '', groovyScript: '''import org.boon.Boon;
 
 def jsonEditorOptions = Boon.fromJson(/{
-        disable_edit_json: true,
+		disable_edit_json: true,
         disable_properties: true,
         no_additional_properties: true,
         disable_collapse: true,
@@ -18,293 +18,35 @@ def jsonEditorOptions = Boon.fromJson(/{
         disable_array_reorder: true,
         theme: "bootstrap2",
         iconlib:"fontawesome4",
-       "schema":{
-  "title": "Applications",
-  "type": "array",
-  "format":"tabs",
-  "items": {
-	 "title": "Application",
-	 "headerTemplate": "{{self.name}}",
-	 "type": "object",
-	 "properties": {
-		"name" : {
-			 "title": "application",
-			 "type": "string",
-			 "readOnly": "true"
-
-		 },
-		 "environments": {
-			 "title": "Environments",
-			 "type": "array",
-			 "format":"tabs",
-			 "items": {
-				 "title": "Environment",
-				 "headerTemplate": "{{self.name}}",
-				 "type": "object",
-				 "properties": {
-					"name" : {
-						 "title": "environment",
-						 "type": "string",
-						 "readOnly": "true"
-
-					 },
-					 "properties": {
-						 "type": "array",
-						 "format": "table",
-						 "items": {
-							 "type": "object",
-							  "properties": {
-								  "name" : {
-									  "type": "string",
-									  "readOnly": "true"
-								  },
-								  "value" : {
-									  "type": "string"
-								  }
-							  }
-						 }
-					 },
-					 "servers": {
-						 "title": "Servers",
-						 "type": "array",
-						 "format":"tabs",
-						 "items": {
-							 "title": "Server",
-							 "headerTemplate": "{{self.name}}",
-							 "type": "object",
-
-							 "properties": {
-								"name" : {
-									 "title": "server",
-									 "type": "string",
-									 "readOnly": "true"
-
-								 },
-								 "properties": {
-									 "type": "array",
-									 "format": "table",
-									 "items": {
-										 "type": "object",
-										  "properties": {
-											  "name" : {
-												  "type": "string",
-												  "readOnly": "true"
-											  },
-											  "value" : {
-												  "type": "string"
-											  },
-											  "override": {
-												"type": "boolean"
-											  }
-										  }
-									 }
-								 }
-							 }
-						 }
-					 }
-				 }
-			 }
-		 }
-	 }
-  }
-},
-       startval: [
-  {
-    "name": "agent",
-    "environments": [
-      {
-        "name": "dev1",
-		"properties": [
-		  {
-			"name": "database_url",
-			"value": "dev1_url"
-		  },
-		  {
-			"name": "database_password",
-			"value": "dev1_password"
-		  }
-		],
-        "servers": [
-			{
-				"name": "agt11",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "agt11_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "agt11_password",
-					"override": "true"
-				  }
-				]
+		schema: {
+		  "type": "object",
+		  "title": "Name",
+		  "properties": {
+			"first_name": {
+			  "type": "string",
+			  "propertyOrder" : 1
 			},
-			{
-				"name": "agt12",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "agt12_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "agt12_password",
-					"override": "true"
-				  }
-				]
-			}
-		]
-      },
-	  {
-        "name": "devprod",
-		"properties": [
-		  {
-			"name": "database_url",
-			"value": "devprod_url"
-		  },
-		  {
-			"name": "database_password",
-			"value": "devprod_password"
-		  }
-		],
-        "servers": [
-			{
-				"name": "agt11",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "agt11_prod_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "agt11_prod_password",
-					"override": "true"
-				  }
-				]
+			"last_name": {
+			  "type": "string",
+			  "propertyOrder" : 2
 			},
-			{
-				"name": "agt12",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "agt12_prod_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "agt12_prod_password",
-					"override": "true"
-				  }
-				]
+			"full_name": {
+			  "type": "string",
+			  "propertyOrder" : 3,
+			  "template": "{{fname}} {{lname}}",
+			  "watch": {
+				"fname": "first_name",
+				"lname": "last_name"
+			  }
 			}
-		]
-      }
-    ]
-  },
-  {
-    "name": "consumer",
-    "environments": [
-      {
-        "name": "dev1",
-		"properties": [
-		  {
-			"name": "database_url",
-			"value": "dev1_url"
-		  },
-		  {
-			"name": "database_password",
-			"value": "dev1_password"
 		  }
-		],
-        "servers": [
-			{
-				"name": "app11",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "app11_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "app11_password",
-					"override": "true"
-				  }
-				]
-			},
-			{
-				"name": "app12",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "app12_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "app12_password",
-					"override": "true"
-				  }
-				]
-			}
-		]
-      },
-	  {
-        "name": "devprod",
-		"properties": [
-		  {
-			"name": "database_url",
-			"value": "devprod_url"
-		  },
-		  {
-			"name": "database_password",
-			"value": "devprod_password"
-		  }
-		],
-        "servers": [
-			{
-				"name": "app11",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "agt11_prod_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "app11_prod_password",
-					"override": "true"
-				  }
-				]
-			},
-			{
-				"name": "agt12",
-				"properties": [
-				  {
-					"name": "database_url",
-					"value": "app12_prod_url",
-					"override": "true"
-				  },
-				  {
-					"name": "database_password",
-					"value": "app12_prod_password",
-					"override": "true"
-				  }
-				]
-			}
-		]
-      }
-    ]
-  }
-]
-}
-
-}/);
-
-return jsonEditorOptions;''', multiSelectDelimiter: ',', name: '', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_JSON', visibleItemCount: 5)], submitter: 'admin', submitterParameter: 'selectedBy'
+		},
+		startval: {
+			"first_name" : "John",
+			"last_name" : "Doe",
+			"full_name" : "John Doe"
+		}
+}/);''', multiSelectDelimiter: ',', name: '', quoteValue: false, saveJSONParameterToFile: false, type: 'PT_JSON', visibleItemCount: 5)], submitter: 'admin', submitterParameter: 'selectedBy'
 		
 	componentList.each{ component ->
 		println "loading version information for groupId=$groupId & artifact Id=$component from $repoUrl";
