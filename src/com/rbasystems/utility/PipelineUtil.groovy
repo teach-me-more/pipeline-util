1583b3f3b060b33c22e375c70b0c74f18bd56181
chaudharyamardeep <chaudharyamardeep@gmail.com>
2020-01-22 19:05:51 -0600

    Added dynamic param

--- src/com/rbasystems/utility/PipelineUtil.groovy
+++ src/com/rbasystems/utility/PipelineUtil.groovy
@@ -4,20 +4,17 @@ import groovy.util.XmlSlurper;
 import groovy.json.JsonSlurper;
 
 class PipelineUtil {
-	static String versionList(productName) {
-		println "Extracting version information for "+productName;
+	static String versionList(repoUrl,groupId,artifactId) {
+		println "Extracting version information for "+artifactId;
 		def XmlSlurper xmlSlurper=new XmlSlurper();
 		def jsonSlurper = new JsonSlurper();
 				
-				def nexusData = xmlSlurper.parse("https://repository.apache.org/service/local/lucene/search?g=commons-logging&a="+productName);
-				def proxyVersions="Select Version";
+				def nexusData = xmlSlurper.parse(repoUrl+groupId+artifactId);
+				def versions="Select Version";
 				nexusData.data.artifact.each { item ->
 					def versionList=""+item.artifactId+"-"+ item.version;
-					println versionList;
-					proxyVersions=proxyVersions+","+versionList;
-				}
-				println "Property: $proxyVersions";
-				
-				return proxyVersions;
+					versions=versions+","+versionList;
+				}				
+				return versions;
 	}
 }
--- vars/extractVersionList.groovy
+++ vars/extractVersionList.groovy
@@ -1,12 +1,26 @@
 #!/usr/bin/env groovy
 import com.rbasystems.utility.PipelineUtil;
 def call(config) {
+	def repoUrl=config["REPO_ROOT_URL"];
+	def groupId=config["GROUP_ID"];
+	def  componentListStr=config["COMPONENT_LIST"];
+	def componentList=componentListStr.split(",");
+	def choiceList = new ArrayList();
+	
+	componentList.each{ component ->
+		println "loading version information for groupId=$groupId & artifact Id=$component from $repoUrl";
+		def versions=PipelineUtil.versionList(repoUrl,groupId,component);
+		
+		def componentVersion = new extendedChoice(description: '', multiSelectDelimiter: ',', name:component, saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', value: versions, visibleItemCount: 2);
+		choiceList.add(choiceList)
+		}
 	println "Hello World try to get version---- $config";
 	
-def versions=PipelineUtil.versionList("commons-logging");
 
 println "version infor in extractVersionList $versions";
-input message: 'Please select a version for deployment', parameters: [extendedChoice(description: '', multiSelectDelimiter: ',', name: 'Version', saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', value: versions, visibleItemCount: 2)], submitter: 'admin', submitterParameter: 'selectedVersion'
+
+input message: 'Please select a version for deployment', parameters: choiceList, submitter: 'admin', submitterParameter: 'selectedVersion'
+//input message: 'Please select a version for deployment', parameters: [extendedChoice(description: '', multiSelectDelimiter: ',', name: 'Version', saveJSONParameterToFile: false, type: 'PT_SINGLE_SELECT', value: versions, visibleItemCount: 2)], submitter: 'admin', submitterParameter: 'selectedVersion'
 
 // def selectedProperty = input (message: 'Select Version', parameters: [choice(choices: proxyVersions, description: 'Allow selecting a version of artifact', name: 'Please select a version of Proxy to deploy')], submitter: 'admin', submitterParameter: 'selectedVersion');
   // def selectedProperty = input( id: 'userInput', message: 'Choose properties file', parameters: [ [$class: 'ChoiceParameterDefinition', choices: proxyVersions, description: 'Properties', name: 'prop'] ])
